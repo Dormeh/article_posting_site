e35c196be4b6e801e0099fce852f6563ae782d53
Dormeh <dormeh@icloud.com>
2023-09-16 17:55:46 +0300

    adds webpack dev server

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -3,23 +3,26 @@ import webpack from "webpack";
 import {buildLoaders} from "./buildLoaders";
 import {buildResolvers} from "./buildResolvers";
 import {buildPlugins} from "./buildPlugins";
+import {buildDebServer} from "./buildDebServer";
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
 
-    const {paths, mode} = options;
+    const {paths, mode, isDev} = options;
 
     return {
         mode,
         entry: paths.entry,
-        module: {
-            rules: buildLoaders()
-        },
-        resolve: buildResolvers(),
         output: {
             filename: "[name].[contenthash].js",
             path: paths.build,
             clean: true,
         },
         plugins: buildPlugins(options),
+        module: {
+            rules: buildLoaders()
+        },
+        resolve: buildResolvers(),
+        devtool: isDev ? 'inline-source-map' : undefined,
+        devServer: isDev ? buildDebServer(options): undefined,
     }
 }
