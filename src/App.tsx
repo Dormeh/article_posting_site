328d52e33a919bddd93044f1069f8dc9263ab58b
Dormeh <dormeh@icloud.com>
2023-09-19 19:46:39 +0300

    Adds routing and lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import {Counter} from "./components/Counter";
 import './index.scss'
+import {Link, Route, Routes} from "react-router-dom";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import MainPage from "./pages/MainPage/MainPage";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 
 const App = () => {
     return (
         <div className="app">
-            sdfsd
-            <Counter/>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />}></Route>
+                    <Route path={'/'} element={<MainPageAsync />}></Route>
+                </Routes>
+            </Suspense>
+
         </div>
     );
 };
