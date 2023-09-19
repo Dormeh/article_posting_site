373a8042f0fca69f601d8771e4a5569f019f2c28
Dormeh <dormeh@icloud.com>
2023-09-19 21:48:53 +0300

    adds styles to change the theme adds theme switch adds theme provider and context adds a custom hook for switching themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import React, {Suspense} from 'react';
-import {Counter} from "./components/Counter";
-import './index.scss'
+import './styles/index.scss'
 import {Link, Route, Routes} from "react-router-dom";
-import AboutPage from "./pages/AboutPage/AboutPage";
-import MainPage from "./pages/MainPage/MainPage";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {useTheme} from "./theme/useTheme";
 
 const App = () => {
+
+    const {theme, toggleTheme} = useTheme()
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync />}></Route>
-                    <Route path={'/'} element={<MainPageAsync />}></Route>
+                    <Route path={'/about'} element={<AboutPageAsync/>}></Route>
+                    <Route path={'/'} element={<MainPageAsync/>}></Route>
                 </Routes>
             </Suspense>
 
