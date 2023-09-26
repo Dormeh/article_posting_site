9280db540f74472eae8fbc58444b8a6225892c4a
Dormeh <dormeh@icloud.com>
2023-09-26 20:17:36 +0300

    adds custom Button component adds svg and file loaders to config webpack

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from './Navbar.module.scss';
 import {AppLink, ApplinkTheme} from "shared/ui/AppLink/AppLink";
+import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
 
 interface NavbarProps {
     className?: string;
@@ -9,9 +10,14 @@ interface NavbarProps {
 export const Navbar = ({className}: NavbarProps) => {
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
+            <ThemeSwitcher />
             <div className={cls.links}>
-                <AppLink theme={ApplinkTheme.SECONDARY} to={'/'}>Главная</AppLink>
-                <AppLink theme={ApplinkTheme.RED} to={'/about'}>О сайте</AppLink>
+                <AppLink theme={ApplinkTheme.SECONDARY} to={'/'}>
+                    Главная
+                </AppLink>
+                <AppLink theme={ApplinkTheme.RED} to={'/about'}>
+                    О сайте
+                </AppLink>
             </div>
         </div>
     );
