21fa065d80554eafe3680dc23a6f6699ee1d44a8
Dormeh <dormeh@icloud.com>
2023-10-10 17:40:45 +0300

    adds componentRender function for testing, adds reg-cli with scripts for screenshot compare

--- src/widgets/Sidebar/ui/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar.test.tsx
@@ -1,15 +1,14 @@
 import { fireEvent, screen } from '@testing-library/react';
 import { Sidebar } from 'widgets/Sidebar/ui/Sidebar';
-import { renderWithTranslation }
-    from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 
 describe('Sidebar', () => {
     test('with only first param', () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
     test('test toggle', () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         const toggle = screen.getByTestId('sidebar-toggle');
         fireEvent.click(toggle);
         expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
