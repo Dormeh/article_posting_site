9e71b0df6cf4d1f3afb1dfb82c588f45b9c7e5a0
Dormeh <dormeh@icloud.com>
2023-10-13 17:54:28 +0300

    changed apply theme class to body

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,27 +1,19 @@
 import React, { Suspense } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 
-const App = () => {
-    const { theme } = useTheme();
+const App = () => (
+    <div className={classNames('app')}>
+        <Suspense fallback="">
+            <Navbar />
+            <div className="content-page">
+                <Sidebar />
+                <AppRouter />
+            </div>
+        </Suspense>
 
-    // document.body.className = theme; // TODO для модального окна в дальнейшем поменять
-
-    return (
-        <div className={classNames('app', {}, [theme])}>
-            <Suspense fallback="">
-                <Navbar />
-                <div className="content-page">
-                    <Sidebar />
-                    <AppRouter />
-                </div>
-            </Suspense>
-
-        </div>
-    );
-};
-
+    </div>
+);
 export default App;
