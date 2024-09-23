2d66effc7d0522e97c6b428f2dca1e4773d0699c
Dormeh <dormeh@icloud.com>
2024-09-23 18:44:04 +0300

    fixes ide found with lint and style code problems

--- src/features/ProfileFormEdit/model/services/fetchProfileData/fetchProfileData.ts
+++ src/features/ProfileFormEdit/model/services/fetchProfileData/fetchProfileData.ts
@@ -4,13 +4,14 @@ import { Profile } from 'entities/Profile';
 import { apiErrorIdentify } from 'shared/api/apiErrorIdentify';
 
 import { ApiErrorTypes } from 'shared/model/consts/api';
+import { AxiosResponse } from 'axios';
 
 export const fetchProfileData = createAsyncThunk<Profile, string, ThunkConfig<string>>(
     'profileFormEdit/fetchProfileData',
     async (profileId, thunkAPI) => {
         const { rejectWithValue, extra, dispatch } = thunkAPI;
         try {
-            const response = await extra.api.get<Profile>(`/profiles/${profileId}`);
+            const response: AxiosResponse<Profile> = await extra.api.get(`/profiles/${profileId}`);
 
             if (!response.data?.id) throw new Error(ApiErrorTypes.DATA_EMPTY_ERROR);
 
