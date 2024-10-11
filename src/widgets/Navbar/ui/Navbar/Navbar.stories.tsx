f4623db84aef9967ef4558826a79de37d777d909
Dormeh <dormeh@icloud.com>
2024-10-11 19:46:21 +0300

    fixes Navbar, NavbarAvatarDropdown stories and screenshots

--- src/widgets/Navbar/ui/Navbar/Navbar.stories.tsx
+++ src/widgets/Navbar/ui/Navbar/Navbar.stories.tsx
@@ -4,6 +4,9 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
 import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
+import withMock from 'storybook-addon-mock';
+import { notificationList } from 'shared/config/tests/constants/notificationList';
+import { Notification } from 'entities/Notification/model/types/notification';
 import { Navbar } from './Navbar';
 
 export default {
@@ -12,17 +15,28 @@ export default {
     argTypes: {
         backgroundColor: { control: 'color' },
     },
+    decorators: [StoreDecorator({}), withMock],
+    parameters: {
+        mockData: [
+            {
+                url: `${__API__}notifications`,
+                method: 'GET',
+                status: 200,
+                response: notificationList as Notification[],
+                delay: 1500,
+            },
+        ],
+    },
 } as ComponentMeta<typeof Navbar>;
 
 const Template: ComponentStory<typeof Navbar> = (args) => <Navbar {...args} />;
 
 export const Light = Template.bind({});
 Light.args = {};
-Light.decorators = [StoreDecorator({})];
 export const Dark = Template.bind({});
 Dark.args = {};
 
-Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({})];
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
 
 export const AuthNavbar = Template.bind({});
 AuthNavbar.args = {};
--- src/widgets/Navbar/ui/NavbarAvatarDropdown/NavbarAvatarDropdown.stories.tsx
+++ src/widgets/Navbar/ui/NavbarAvatarDropdown/NavbarAvatarDropdown.stories.tsx
@@ -1,6 +1,9 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
+import { HStack } from 'shared/ui/Stack';
+import { UserRole } from 'shared/model/consts/common';
 import { NavbarAvatarDropdown } from './NavbarAvatarDropdown';
 
 export default {
@@ -9,6 +12,14 @@ export default {
     argTypes: {
         backgroundColor: { control: 'color' },
     },
+    decorators: [
+        StoreDecorator({}),
+        (Story) => (
+            <HStack style={{ padding: '100px' }} max={false}>
+                <Story />
+            </HStack>
+        ),
+    ],
 } as ComponentMeta<typeof NavbarAvatarDropdown>;
 
 const Template: ComponentStory<typeof NavbarAvatarDropdown> = (args) => (
@@ -17,6 +28,9 @@ const Template: ComponentStory<typeof NavbarAvatarDropdown> = (args) => (
 
 export const Primary = Template.bind({});
 Primary.args = {};
+
+export const PrimaryAuth = Template.bind({});
+PrimaryAuth.decorators = [StoreDecorator({ user: { authData: { roles: [UserRole.ADMIN] } } })];
 
 export const Dark = Template.bind({});
 Dark.args = {};
